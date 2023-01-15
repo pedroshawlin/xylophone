3a3b6b2b512b92201088995f0d0ea10e0c5d25cf
Pedro Almeida <pedrofcastro@hotmail.com>
2023-01-15 02:44:11 -0300

    xylophone

---
+++ script.js
@@ -0,0 +1,24 @@
+const UInotes = document.querySelector('#notes')
+
+window.addEventListener('keydown' , (e) => {
+    playNotes(e.key)
+} )
+
+
+UInotes.addEventListener('click',(e)=> {
+    if(e.target.parentElement.id === 'notes'){
+        playNotes(e.target.dataset.key)
+    }
+})
+
+
+function playNotes(key) {
+    const notes = document.querySelector(`audio[data-key="${key}"]`)
+
+    if (!notes) {
+        return
+    }
+
+    notes.currentTime = 0
+    notes.play()
+}
